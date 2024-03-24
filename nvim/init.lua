01cb8cd7bad880e1c72587196c54c8a694cb74f2
papp-git <164748983+papp-git@users.noreply.github.com>
2024-03-24 13:56:54 +0100

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,6 @@ require "config/lazy"
 require "config/options"
 require "config/keymappings"
 require "config/luasnip"
---require('lualine').setup()
 require "config/nvim_cmp"
 require "config/lualine"
 require "config/tree"
