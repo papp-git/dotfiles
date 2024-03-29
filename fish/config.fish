bde768be795cc88bc60ada9098a0216f3a1f421a
papp <s.papp@gmx.net>
2024-03-29 11:13:52 +0100

    update

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# fish_greeting
+function fish_greeting
+    echo Zeit: (set_color yellow; date +%T; set_color blue)  $hostname
+end
+
+# aliases
+alias ticker="ticker --config ~/ticker.yaml"
---
+++ latex/.latexmkrc
@@ -0,0 +1,3 @@
+$pdf_mode = 4;	#lualatex
+$show_time = 1;	# zeige CPU-Zeit
+# set_tex_cmds("--shell-escape --synctex=1 %0 %S");
